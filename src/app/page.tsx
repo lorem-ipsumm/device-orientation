80b4f39c6e3b6513021e2d6d87a11fc9a199d7a2
lorem ipsum <lorem.ipsum.crypto@gmail.com>
2023-12-27 21:09:58 -0500

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,142 @@
+"use client";
+import { useEffect, useState } from "react";
+
+declare global {
+  interface Window {
+    DeviceOrientationEvent: typeof DeviceOrientationEvent & {
+      requestPermission?: () => Promise<string>;
+    };
+  }
+}
+
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+let position:Coordinate = {x: 50, y: 50};
+let velocity:Coordinate = {x: 0, y: 0};
+let ballProperties = {
+  radius: 50,
+  width: 50,
+  height: 50,
+  color: "#1070a3",
+  // x: 50,
+  // y: 50,
+  // vx: 0,
+  // vy: 0,
+  friction: 0.5,
+  maxVelocity: 3,
+}
+
+export default function HomePage() {
+
+  const [rotation, setRotation] = useState<number>(0);
+  const [leftToRight, setLeftToRight] = useState<number>(0);
+  const [frontToBack, setFrontToBack] = useState<number>(0);
+
+  const renderVar = (name: string, value: number) => {
+    return (
+      <div>
+        <span>{name}: {value.toFixed(2)}</span>
+      </div>
+    )
+  }
+
+  const handleDeviceOrientation = (event: any) => {
+    // alpha: rotation around z-axis
+    const rotateDegrees = event.alpha;
+    // gamma: left to right
+    const leftToRight = event.gamma;
+    // beta: front back motion
+    const frontToBack = event.beta;
+
+    setRotation(rotateDegrees || 0);
+    setLeftToRight(leftToRight || 0);
+    setFrontToBack(frontToBack || 0);
+    velocity = handleVelocity(frontToBack, leftToRight);
+    position = handlePosition();
+  };
+
+  const handlePosition = () => {
+    // Update the ball's position
+    let newX = position.x + velocity.x;
+    let newY = position.y + velocity.y;
+
+    // Ensure that the ball's position stays within the 0-100% range
+    newX = Math.max(0, Math.min(100, newX));
+    newY = Math.max(0, Math.min(100, newY));
+
+    return { x: newX, y: newY };
+  }
+
+  const handleVelocity = (frontToBack: number, leftToRight: number) => {
+
+    const x = position.x;
+    const y = position.y;
+    const prevVelocity = velocity;
+
+    const {
+      friction,
+      maxVelocity,
+    } = ballProperties;
+
+    let newVX = prevVelocity.x + leftToRight / 100;
+    let newVY = prevVelocity.y + frontToBack / 100;
+    // ensure the velocity stays within the maxVelocity and -maxVelocity
+    newVX = Math.max(-maxVelocity, Math.min(maxVelocity, newVX));
+    newVY = Math.max(-maxVelocity, Math.min(maxVelocity, newVY));
+
+    if (y + prevVelocity.y < 0 || y + prevVelocity.y > 100) {
+      newVY = -newVY * friction;
+    }
+    if (x + prevVelocity.x < 0 || x + prevVelocity.x > 90) {
+      newVX = -newVX * friction;
+    }
+
+    return { x: newVX, y: newVY };
+  }
+  
+  const requestPermission = async () => {
+    // create a event listener that listens device orientation change
+    if (window.DeviceOrientationEvent) {
+      // Check if we need to request permission for iOS 13+
+      if (typeof window.DeviceOrientationEvent.requestPermission === 'function') {
+        // Handle user interaction
+        const permissionState = await (window.DeviceOrientationEvent as any).requestPermission();
+        alert(permissionState)
+        if (permissionState === 'granted') {
+          window.addEventListener("deviceorientation", handleDeviceOrientation);
+        }
+      } else {
+        window.addEventListener("deviceorientation", handleDeviceOrientation);
+      }
+    }
+    // reset vars
+    velocity = {x: 0, y: 0};
+    position = {x: 50, y: 50};
+  }
+
+  return (
+    <main 
+      className="flex min-h-screen flex-col items-center justify-center bg-zinc-900 text-white"
+      onClick={requestPermission}
+    >
+      <div className="flex flex-col gap-4">
+        {renderVar("x", position.x)}
+        {renderVar("y", position.y)}
+        {renderVar("velocity.x", velocity.x)}
+        {renderVar("velocity.y", velocity.y)}
+      </div>
+      <div id="ball" style={{
+        position: 'absolute',
+        top: `${position.y}%`,
+        left: `${position.x}%`,
+        width: `${ballProperties.width}px`,
+        height: `${ballProperties.height}px`,
+        borderRadius: '50%',
+        backgroundColor: '#1070a3',
+      }}></div>
+    </main>
+  );
+}
